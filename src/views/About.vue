3287ffbe397e46c46f5acb2bfe2c8fbebbb4ec81
Aitor <aitor.ramirez@cargobot.io>
2026-05-20 14:45:39 +0100

    Added Resume Download Button

--- src/views/About.vue
+++ src/views/About.vue
@@ -52,6 +52,36 @@
             </svg>
             aitor.dev
           </a>
+
+          <a
+            class="section-container-nav-icons"
+            href="ITResume.pdf"
+            download="Aitor-Ramirez-Resume.pdf"
+          >
+
+          
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="icon icon-tabler icons-tabler-outline icon-tabler-file-cv"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+              <path
+                d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2"
+              />
+              <path d="M11 12.5a1.5 1.5 0 0 0 -3 0v3a1.5 1.5 0 0 0 3 0" />
+              <path d="M13 11l1.5 6l1.5 -6" />
+            </svg>
+            Resume
+          </a>
         </nav>
       </div>
 
@@ -115,7 +145,11 @@
         <div class="section-project-card-container">
           <div class="section-project-card-container-title">
             <h3>Web Page</h3>
-            <a href="https://github.com/Yerkos/mi-cv" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/Yerkos/mi-cv"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
@@ -140,7 +174,11 @@
         <div class="section-project-card-container">
           <div class="section-project-card-container-title">
             <h3>AppleTrack</h3>
-            <a href="https://github.com/Yerkos/appletack" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/Yerkos/appletack"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 24 24"
